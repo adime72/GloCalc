7acbf93ef76f4941687831032b840c0a2197b45a
marinarimon <marinarimon@gmail.com>
2017-11-28 00:11:20 +0200

    tafrit + home.html + user

--- App/app.js
+++ App/app.js
@@ -1,23 +1,18 @@
-var gloCalc = angular.module("GloCalc", ["ngRoute"]);
+var recipeApp = angular.module("GloCalc", ["ngRoute"]);
 
-
-gloCalc.config(function ($routeProvider) {
+recipeApp.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "App/home/home.html"
-          //  controller: "HomeCtrl"
+            //controller: "HomeCtrl"
         })
-       .when("/signUp", {
-       templateUrl: "App/signUp/signUp.html"
-        //     controller: "RecipeGalleryCtrl"
-       })
+        .when("/signUp", {
+            templateUrl: "App/signUp/signUp.html" 
+            //controller: "RecipeGalleryCtrl"
+        })
         .when("/users", {
-        templateUrl: "App/users/users.html"
-        //     controller: "RecipeDetailsCtrl"
-        })
-        //.when("/new", {
-        //     templateUrl: "app/recipe/newRecipe.html",
-        //     controller: "NewRecipeCtrl"            
-        // })
+            templateUrl: "App/users/users.html"
+            //controller: "RecipeDetailsCtrl"
+        })});
 
-});
+
