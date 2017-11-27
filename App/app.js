0426d23ab3e017677be8ec7ac27e12d62ef8f91d
MarinaRimon <marinarimon@gmail.com>
2017-11-27 12:59:43 +0200

    router setup

--- App/app.js
+++ App/app.js
@@ -1,1 +1,22 @@
-var recipeApp = angular.module("GloCalc",[]);
+var gloCalc = angular.module("GloCalc", ["ngRoute"]);
+
+
+gloCalc.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "App/home/home.html"
+          //  controller: "HomeCtrl"
+        })
+       .when("/signUp", {
+       templateUrl: "app/signUp/signUp.html",
+        //     controller: "RecipeGalleryCtrl"
+        // })
+        // .when("/recipes/:recipeIndex", {
+        //     templateUrl: "app/recipe/recipeDetails.html",
+        //     controller: "RecipeDetailsCtrl"
+        // }).when("/new", {
+        //     templateUrl: "app/recipe/newRecipe.html",
+        //     controller: "NewRecipeCtrl"            
+        // })
+
+});
--- app/home/homeCtrl.js
+++
@@ -1,8 +0,0 @@
-recipeApp.controller("HomeCtrl", function ($scope, $uibModal) {
-    $scope.login = function() {
-        $uibModal.open({
-            templateUrl: "app/login/login.html",
-            controller: "LoginCtrl"
-        })
-    }
-});
